bc58dc52e22dd855f77d2eb7d911792cc5188614
goleasmita <goleasmita876@gmail.com>
2025-09-29 18:48:21 +0530

    Sigma

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 👇 This ensures assets and routes work when deployed under /sigma_frontend/
-  base: "/sigma_frontend/",
-  server: {
-    port: 5173, // optional: set a fixed port for local dev
-    open: true, // optional: auto-open browser
-  },
-  build: {
-    outDir: "dist", // default build output folder
-  },
+  base: "/sigma_frontend/", // 👈 your repo name
 });
